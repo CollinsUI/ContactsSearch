8bbe19aec83f9b1b0943ce596598d668ee412686
Collins Swift <91741002+CollinsUI@users.noreply.github.com>
2022-12-07 20:31:53 +0300

    Initial Commit

---
+++ ContactsSearch/ContactsSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsSearchApp.swift
+//  ContactsSearch
+//
+//  Created by Collins Muthomi on 07/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactsSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
